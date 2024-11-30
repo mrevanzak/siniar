08f762071230b01bdee0f3188fdbd0e366a62f71
mrevanzak <mrevanzak@gmail.com>
2024-11-30 13:09:21 +0700

    feat: slider functionality

--- src/app/player.tsx
+++ src/app/player.tsx
@@ -8,13 +8,20 @@ import {
   Sound,
 } from 'iconsax-react-native';
 import { useColorScheme } from 'nativewind';
+import { useCallback, useState } from 'react';
+import type { LayoutChangeEvent } from 'react-native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 import Animated, {
+  clamp,
+  Easing,
+  runOnJS,
   useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
   withTiming,
 } from 'react-native-reanimated';
 import TrackPlayer, {
   useActiveTrack,
-  useIsPlaying,
   useProgress,
 } from 'react-native-track-player';
 
@@ -38,13 +45,48 @@ export default function PlayerModal() {
 
   const activeTrack = useActiveTrack();
   const { duration, position } = useProgress();
-  const { playing } = useIsPlaying();
 
-  const playbackStyle = useAnimatedStyle(() => ({
-    width: withTiming(playing ? `${(position / duration) * 100}%` : '0%', {
-      duration: 1000,
-    }),
-  }));
+  const [sliderWidth, setSliderWidth] = useState(0);
+  const onSliderContainerLayout = useCallback((event: LayoutChangeEvent) => {
+    setSliderWidth(event.nativeEvent.layout.width);
+  }, []);
+
+  async function seekAndPlay(offsetPercentage: number, duration: number) {
+    await TrackPlayer.seekTo(offsetPercentage * duration);
+    TrackPlayer.play();
+    isInteracting.value = false;
+  }
+
+  const isInteracting = useSharedValue(false);
+  const offset = useSharedValue(0);
+  const gesture = Gesture.Pan()
+    .onStart(() => {
+      isInteracting.value = true;
+      runOnJS(TrackPlayer.pause)();
+    })
+    .onUpdate((event) => {
+      offset.value = clamp(event.x, 0, sliderWidth);
+    })
+    .onEnd(() => {
+      const offsetPercentage = offset.value / sliderWidth;
+      runOnJS(seekAndPlay)(offsetPercentage, duration);
+    });
+
+  const progress = useDerivedValue(() => {
+    if (isInteracting.value) {
+      return (offset.value / sliderWidth) * 100;
+    }
+    return duration > 0 ? (position / duration) * 100 : 0;
+  });
+
+  const playbackStyle = useAnimatedStyle(() => {
+    return {
+      width: withTiming(`${progress.value}%`, {
+        duration: isInteracting.value ? 0 : 500,
+        easing: Easing.linear,
+      }),
+    };
+  });
 
   return (
     <BlurView
@@ -86,21 +128,26 @@ export default function PlayerModal() {
               {formatDuration(duration)}
             </Text>
           </View>
-          <View className="overflow-hidden">
-            <View className="flex-row">
-              {Array.from({ length: 10 }).map((_, index) => (
-                <Sound key={index} size={48} color={color} />
-              ))}
-            </View>
-            <Animated.View
-              className="absolute inset-x-0 flex-row overflow-hidden"
-              style={playbackStyle}
+          <GestureDetector gesture={gesture}>
+            <View
+              className="overflow-hidden"
+              onLayout={onSliderContainerLayout}
             >
-              {Array.from({ length: 10 }).map((_, index) => (
-                <Sound key={index} size={48} color={colors['primary-red']} />
-              ))}
-            </Animated.View>
-          </View>
+              <View className="flex-row">
+                {Array.from({ length: 10 }).map((_, index) => (
+                  <Sound key={index} size={48} color={color} />
+                ))}
+              </View>
+              <Animated.View
+                className="absolute inset-x-0 flex-row overflow-hidden"
+                style={playbackStyle}
+              >
+                {Array.from({ length: 10 }).map((_, index) => (
+                  <Sound key={index} size={48} color={colors['primary-red']} />
+                ))}
+              </Animated.View>
+            </View>
+          </GestureDetector>
         </View>
 
         <View className="flex-row items-center justify-center gap-5">
